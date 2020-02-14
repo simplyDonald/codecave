f339a6078d829728d0082903cc3633e9138037ea
simplyDonald <donaldabuah@gmail.com>
2020-02-14 09:09:53 -0700

    implemented Dynamic ticker option and error handling

--- askBinance.py
+++ askBinance.py
@@ -7,17 +7,16 @@ import ssl
 # api_key = 'AIzaSy___IDByT70'
 
 '''
-askBinance v1.0
+askBinance v1.1
 by Donald Abuah
 Free software for real-time bitcoin price analysis and comparison with Nigerian Naira (NGN)
 *Upcoming features
 - Telegram bot feature
-- Dynamic Ticker options
 donaldabuah@gmail.com
 '''
 
 
-serviceurl = 'https://api.binance.com/api/v3/ticker/bookTicker'
+serviceurl = 'https://api.binance.com/api/v3/ticker/bookTicker?'
 
 
 # Ignore SSL certificate errors
@@ -25,45 +24,81 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-for i in range(1):
-    '''
+while True:
+
     cryptoinput = input('Enter ticker-symbol(e.g BTCUSDT): ')
     cryptopair = cryptoinput.upper()
-    if len(cryptoinput) < 1: break
+    if len(cryptoinput) < 1:
+        print('There\'s so much SPACE between us,',end ='')
+        print(' please input a value :)')
+        continue
 
     parms = dict()
     parms['symbol'] = cryptopair
     #if api_key is not False: parms['key'] = api_key
-    '''
-    #url = serviceurl + urllib.parse.urlencode(parms)
 
-    print('Retrieving', serviceurl)
-    uh = urllib.request.urlopen(serviceurl, context=ctx)
+    url = serviceurl + urllib.parse.urlencode(parms)
+    url2 = 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCNGN'
+
+    print('Retrieving', url)
+    try:
+        uh = urllib.request.urlopen(url, context=ctx)
+    except urllib.error.HTTPError:
+        print('Oops!  that was no valid symbol. Can you try something else?..')
+        continue
+
+
     data = uh.read().decode()
+    uh2 = urllib.request.urlopen(url2, context=ctx)
+    data2 = uh2.read().decode()
     print('Retrieved', len(data), 'characters')
 
-    #try:
-    #    js = json.loads(data)
-    #except:
-    #    js = None
 
-    '''
-        if not js or 'status' not in js or js['status'] != 'OK':
-        print('==== Failure To Retrieve ====')
-        print(data)
-        continue
-    '''
     js = json.loads(data)
+    js2 = json.loads(data2)
+
+
+    #if 'symbol' not in js:
+        #print('==== Failure To Retrieve ====')
+        #print(js['message'])
+        #continue
+
+
+    print('+++++++++++++++++++++++++')
     #print(json.dumps(js, indent=4))
+
+
+    NAIRABASE = (float(js2['bidPrice']))
+    CNPRICE = (float(js['askPrice']))
+    EXCHNG = NAIRABASE / CNPRICE
+    EXCHNG = str(round(EXCHNG,3))
+    NAIRABASE = str(round(NAIRABASE,3))
+    CNPRICE = str(round(CNPRICE,3))
+    CNSYMB = (js['symbol'])
     print('+++++++++++++++++++++++++')
-    USD = (js[11]['askPrice'])
-    CNSYMB = (js[11]['symbol'])
-    print('1 BTC =$'+ USD, CNSYMB)
-    #print(js[11]['symbol'])
-    print('+++++++++++++++++++++++++')
-    NGN = (js[651]['bidPrice'])
-    CNSYMB2 = (js[651]['symbol'])
-    print('1 BTC is =N=' + NGN, CNSYMB2)
-    print('@@@@@@@@@@@@@@@@@@@@@@@@')
-    EXCHNG = float(NGN) / float(USD)
-    print('RATE =N=' + str(EXCHNG))
+    print('''@@@@@@@@@@@@@@@@@@@@@@&(*,*(@@@@@@@@@
+@@@@@@@@@@@@@@*,......,,,.........,@&@/////%@@@@@@
+@@@@@@@@@@,.,..,,,,,,..,,#.,.,..&&&&&&&&&&//@@@@@@
+@@@@@@@/......,,,,,,,,,,,,,,,.@&&..,*,.&&&&@@@@@@@
+@@@@@,,..,&%(,.,,,,,,,,,,,,..&&&,,&&&&.,&&@,.#@@@@
+@@@&....,,,...,..,*%,.,,,.,&@&&@&.,..,.&&&,...,@@@
+@@*,,,,,,,,,,,,,,,........&@&&&&&&&&&&&@.,,.....@@
+@(......,,,,,,....../////&&&@&@&@&@&&&,,,....,...@
+@,......,,,,,,...,//////@&&@&/&@&&&@,...,,,,,,..,.
+#.,,...,,,,,,,..,///,,////%//#&&&&//....,,,,,,,,..
+,.,,..,.....,...//,,..&%//////&&////..,.,,,,,,,,,.
+%.,,,,,..,,*.........&%#//#%///.////.,..,,,,,,,,.,
+@,..,,,.,..,,..,,,,,&%%/%%%(.,,///..,,,,,,,...,.,.
+@%...,..,.,....,,,.&%%%%&/,..*//,...,,,,,,,../&*.@
+@@@@&&&&&&&/,..,..&&&&&,......,..,,.,,,,,,,..,.,@@
+@@@@@&&#%%%&&&@&,..,....,.,,,,,,,,,,,,,,,,,,,,,@@@
+@@@@@&@&&&&&&@&%%@@#,.,,,,,,,...*.,.,,,,,,,.,@@@@@
+@@@@@@@@&@&&&%%%&&&&&@,...,,,..,*...,,,....@@@@@@@
+@@@@@@@@@@@&@&%%&&&&&&@&,.,,,,,,,,,,,,,.@@@@@@@@@@
+@@@@@@@@@@@@@@@&&&&&&&&&&.,.........@@@@@@@@@@@@@@
+------------------TO THE MOON!!!!!----------------
+    '''
+    )
+    print('1',CNSYMB,'=$' + CNPRICE)
+    print('1 BTC is also =N=' + NAIRABASE)
+    print('RATE =N=' + EXCHNG)
