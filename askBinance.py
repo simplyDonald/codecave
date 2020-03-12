0ac52c10cfdd54f3554a3da417472b81274be60a
simplyDonald <donaldabuah@gmail.com>
2020-03-12 08:37:03 -0400

    Implemented 24hr ticker feature and default value of btcusdt on hitting the enter key

--- askBinance.py
+++ askBinance.py
@@ -17,7 +17,7 @@ donaldabuah@gmail.com
 
 
 serviceurl = 'https://api.binance.com/api/v3/ticker/bookTicker?'
-
+tickerurl = 'https://api.binance.com/api/v3/ticker/24hr?'
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -29,15 +29,15 @@ while True:
     cryptoinput = input('Enter ticker-symbol(e.g BTCUSDT): ')
     cryptopair = cryptoinput.upper()
     if len(cryptoinput) < 1:
-        print('There\'s so much SPACE between us,',end ='')
-        print(' please input a value :)')
-        continue
+        cryptopair = 'BTCUSDT'
+
 
     parms = dict()
     parms['symbol'] = cryptopair
     #if api_key is not False: parms['key'] = api_key
 
     url = serviceurl + urllib.parse.urlencode(parms)
+    tickerurl = tickerurl + urllib.parse.urlencode(parms)
     url2 = 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCNGN'
     RATEURL = 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=BUSDNGN'
 
@@ -52,6 +52,8 @@ while True:
     data = uh.read().decode()
     uh2 = urllib.request.urlopen(url2, context=ctx)
     data2 = uh2.read().decode()
+    th = urllib.request.urlopen(tickerurl, context=ctx)
+    dataticker = th.read().decode()
     RATEURL = urllib.request.urlopen(RATEURL, context=ctx)
     RATEDATA = RATEURL.read().decode()
     print('Retrieved', len(data), 'characters')
@@ -60,6 +62,7 @@ while True:
     js = json.loads(data)
     js2 = json.loads(data2)
     js3 = json.loads(RATEDATA)
+    js4 = json.loads(dataticker)
 
 
     #if 'symbol' not in js:
@@ -83,6 +86,8 @@ while True:
     NAIRABASE = str(round(NAIRABASE,3))
     CNPRICE = str(round(CNPRICE,3))
     CNSYMB = (js['symbol'])
+    tickdat = js4['priceChangePercent']
+    tickdat = str(tickdat)
     print('+++++++++++++++++++++++++')
     print('''@@@@@@@@@@@@@@@@@@@@@@&(*,*(@@@@@@@@@
 @@@@@@@@@@@@@@*,......,,,.........,@&@/////%@@@@@@
@@ -107,7 +112,7 @@ while True:
 ------------------TO THE MOON!!!!!----------------
     '''
     )
-    print('1',CNSYMB,'=$' + CNPRICE)
+    print('1',CNSYMB,'=$' + CNPRICE,'\t24hr change:' + tickdat + '%')
     print('1 BTC is also =N=' + NAIRABASE)
 
     if 'USDT' not in cryptopair:
