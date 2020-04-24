9d81a205c0df088a575212f891e16713f0f455eb
simplyDonald <donaldabuah@gmail.com>
2020-04-24 12:31:46 -0400

    switched to requests module

--- askBinance.py
+++ askBinance.py
@@ -1,6 +1,4 @@
-import urllib.request, urllib.parse, urllib.error
-import json
-import ssl
+import requests
 
 #api_key = 'tQNzkr*******gd8gY'
 # If you have an API key, enter it here
@@ -16,13 +14,8 @@ donaldabuah@gmail.com
 '''
 
 
-serviceurl = 'https://api.binance.com/api/v3/ticker/bookTicker?'
-tickerurl = 'https://api.binance.com/api/v3/ticker/24hr?'
-
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+serviceurl = 'https://api.binance.com/api/v3/ticker/bookTicker'
+tickerurl = 'https://api.binance.com/api/v3/ticker/24hr'
 
 while True:
 
@@ -36,45 +29,28 @@ while True:
     parms['symbol'] = cryptopair
     #if api_key is not False: parms['key'] = api_key
 
-    url = serviceurl + urllib.parse.urlencode(parms)
-    tickerurl = tickerurl + urllib.parse.urlencode(parms)
-    url2 = 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCNGN'
-    RATEURL = 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=BUSDNGN'
-
-    print('Retrieving', url)
     try:
-        uh = urllib.request.urlopen(url, context=ctx)
-    except urllib.error.HTTPError:
-        print('Oops!  that was no valid symbol. Can you try something else?..')
+        url = requests.get('https://api.binance.com/api/v3/ticker/bookTicker', params=parms)
+        tickerurl = requests.get('https://api.binance.com/api/v3/ticker/24hr', params=parms)
+    except error as e:
+        print(e)
         continue
 
-    try:
-        th = urllib.request.urlopen(tickerurl, context=ctx)
-    except urllib.error.HTTPError as e:
-        if e.code == 400:
-            print('ZZZzzzzzz....\nServer was sleeping,pls try again')
-            continue
+    url2 = requests.get('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCNGN')
+    RATEURL = requests.get('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BUSDNGN')
 
-    data = uh.read().decode()
-    uh2 = urllib.request.urlopen(url2, context=ctx)
-    data2 = uh2.read().decode()
-    dataticker = th.read().decode()
-    RATEURL = urllib.request.urlopen(RATEURL, context=ctx)
-    RATEDATA = RATEURL.read().decode()
-    print('Retrieved', len(data), 'characters')
+    print('Retrieving', url.url)
+
+    js = url.json()
+    js2 = url2.json()
+    js3 = RATEURL.json()
+    js4 = tickerurl.json()
 
 
-    js = json.loads(data)
-    js2 = json.loads(data2)
-    js3 = json.loads(RATEDATA)
-    js4 = json.loads(dataticker)
-
-
-    #if 'symbol' not in js:
-        #print('==== Failure To Retrieve ====')
-        #print(js['message'])
-        #continue
-
+    if 'symbol' not in js:
+        print('==== Oops! there was a glitch somewhere... ====')
+        print(js['msg'])
+        continue
 
     print('+++++++++++++++++++++++++')
     #print(json.dumps(js, indent=4))
